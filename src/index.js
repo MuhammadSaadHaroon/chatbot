952eb97e40c5ce9cd9512c6877ec31ba5c76638a
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-11-27 07:50:34 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import ReactDOM from "react-dom/client";
 import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom"; // use HashRouter for GitHub Pages
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <HashRouter>
